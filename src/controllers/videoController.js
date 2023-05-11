807eb0352a30e9965fdad3c30b5011caf33833bc
moonk <mkjoehsb@gmail.com>
2023-05-11 11:42:38 +0900

    7. pug mixins 사용법

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,33 @@
-export const handleHome = (req, res) =>
-  res.render("home", { pageTitle: "Home" });
+// const videos = [1, 2, 3, 4, 5, 6];
+const videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: "excellent",
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: "nice",
+    createdAt: "3 minutes ago",
+    views: 59,
+    id: 1,
+  },
+];
+export const handleHome = (req, res) => {
+  return res.render("home", { pageTitle: "Home", videos });
+};
 export const handleWatchVideo = (req, res) =>
   res.render("watch", { pageTitle: "Watch" });
 
