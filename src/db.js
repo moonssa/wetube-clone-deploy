d07e8334c29a9024f4a14ad514d947724e46f128
moonk <mkjoehsb@gmail.com>
2023-05-12 12:13:24 +0900

    Revert "mongo db connect"
    This reverts commit bd62d208b5a0379640f87af12f44fbca69b4c999.

--- src/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-mongoose
-  .connect("mongodb://127.0.0.1:27017/mytube")
-  .then(() => console.log("✅Connected to DB"))
-  .catch((error) => console.log("❌ DB error : ", error));
