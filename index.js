d033608e197e513eb4527ed1ff2f936fbeff4431
moonk <mkjoehsb@gmail.com>
2023-05-12 13:18:03 +0900

    10. install mongoose and then , mongoDB connect & makes Video schema

--- index.js
+++
@@ -1,2 +0,0 @@
-console.log("Hello JS");
-console.log("setup complete");
---
+++ src/db.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+mongoose.connect("mongodb://127.0.0.1:27017/mytube");
+const db = mongoose.connection;
+
+const handleError = (error) => console.log("❌ DB Error:", error);
+const handleOpen = () => console.log("✅ DB Connect");
+
+db.on("error", handleError);
+db.once("open", handleOpen);
