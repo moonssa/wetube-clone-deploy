0bd59cffcb397e439d5e5afc6981ef91859d4a3c
moonk <mkjoehsb@gmail.com>
2023-05-13 12:20:52 +0900

    13. get/post edit

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -5,19 +5,39 @@ export const home = async (req, res) => {
   return res.render("home", { pageTitle: "Home", videos });
 };
 
-export const handleWatchVideo = async (req, res) => {
+export const watch = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found !" });
+  }
   return res.render("watch", { pageTitle: video.title, video });
 };
 
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
   const { id } = req.params;
-
-  return res.render("edit", { pageTitle: `Edit:` });
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: `Edit Failed!` });
+  }
+  return res.render("edit", { pageTitle: `Edit`, video });
 };
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
   const { id } = req.params;
+  const { title, description, hashtags } = req.body;
+
+  const video = await Video.exists({ _id: id });
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found!" });
+  }
+  await Video.findByIdAndUpdate(id, {
+    title,
+    description,
+    hashtags: hashtags
+      .split(",")
+      .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+  });
+
   return res.redirect(`/videos/${id}`);
 };
 
@@ -32,7 +52,9 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      hashtags: hashtags.split(",").map((word) => `#${word}`),
+      hashtags: hashtags
+        .split(",")
+        .map((word) => (word.startsWith("#") ? word : `#${word}`)),
       meta: {
         views: 0,
         rating: 0,
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,7 +1,7 @@
 import express from "express";
 
 import {
-  handleWatchVideo,
+  watch,
   handleDeleteVideo,
   getEdit,
   postEdit,
@@ -10,7 +10,7 @@ import {
 } from "../controllers/videoController";
 const videoRouter = express.Router();
 
-videoRouter.get("/:id([a-f0-9]{24})", handleWatchVideo);
+videoRouter.get("/:id([a-f0-9]{24})", watch);
 videoRouter.route("/:id([a-f0-9]{24})/edit").get(getEdit).post(postEdit);
 videoRouter.get("/:id([a-f0-9]{24})/delete", handleDeleteVideo);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
