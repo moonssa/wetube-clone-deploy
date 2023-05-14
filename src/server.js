428eae8614f48275db8e847afababa9788d35d2b
moonk <mkjoehsb@gmail.com>
2023-05-15 08:50:10 +0900

    19. session 사용법

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import session from "express-session";
+
 import morgan from "morgan";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
@@ -11,6 +13,26 @@ app.set("views", process.cwd() + "/src/views");
 
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: "Hello",
+    resave: true,
+    saveUninitialized: true,
+  }),
+);
+
+app.use((req, res, next) => {
+  req.sessionStore.all((error, sessions) => {
+    console.log(sessions);
+    next();
+  });
+});
+
+app.get("/add-one", (req, res) => {
+  req.session.potato += 1;
+  return res.send(`${req.session.id} ${req.session.potato}`);
+});
 
 app.use("/", rootRouter);
 app.use("/users", userRouter);
