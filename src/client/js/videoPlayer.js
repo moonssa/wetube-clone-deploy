1ab2e3baf126896e231915da08f8158da42473ef
moonk <mkjoehsb@gmail.com>
2023-05-30 10:03:30 +0900

    video style

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,12 +1,17 @@
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
+const playBtnIcon = playBtn.querySelector("i");
+
 const muteBtn = document.getElementById("mute");
+const muteBtnIcon = muteBtn.querySelector("i");
+
 const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
 const currentTime = document.getElementById("currentTime");
 const totalTime = document.getElementById("totalTime");
 const timeline = document.getElementById("timeline");
 const fullScreenBtn = document.getElementById("fullScreen");
+const fullScreenIcon = fullScreenBtn.querySelector("i");
 const videoContainer = document.getElementById("videoContainer");
 const videoControls = document.getElementById("videoControls");
 
@@ -21,7 +26,8 @@ const handlePlayClick = (e) => {
   } else {
     video.pause();
   }
-  playBtn.innerText = video.paused ? "Play" : "Paused";
+  // playBtn.innerText = video.paused ? "Play" : "Paused";
+  playBtnIcon.classList = video.paused ? "fas fa-play" : "fas fa-pause";
 };
 
 const handleMute = (e) => {
@@ -30,7 +36,11 @@ const handleMute = (e) => {
   } else {
     video.muted = true;
   }
-  muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+  // muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+  muteBtnIcon.classList = video.muted
+    ? "fas fa-volume-mute"
+    : "fas fa-volume-up";
+
   volumeRange.value = video.muted ? 0 : volumeValue;
 };
 
@@ -38,7 +48,8 @@ const handleVolumeChange = (event) => {
   const { value } = event.target;
   if (video.muted) {
     video.muted = false;
-    muteBtn.innerText = "Mute";
+    // muteBtn.innerText = "Mute";
+    muteBtnIcon.classList = "fas fa-volume-up";
   }
 
   video.volume = value;
@@ -71,10 +82,10 @@ const handleFullScreen = () => {
   const fullscreen = document.fullscreenElement;
   if (fullscreen === null) {
     videoContainer.requestFullscreen();
-    fullScreenBtn.innerText = "Exit Full creen";
+    fullScreenIcon.classList = "fas fa-compress";
   } else {
     document.exitFullscreen();
-    fullScreenBtn.innerText = "Enter Full Screen";
+    fullScreenIcon.classList = "fas fa-expand";
   }
 };
 
