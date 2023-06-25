59159870e09feef0ee97df6a070b0fd6d3d0dacb
moonk <mkjoehsb@gmail.com>
2023-06-25 10:23:33 +0900

    download recorder file

--- src/client/js/videoRecorder.js
+++ src/client/js/videoRecorder.js
@@ -1,26 +1,50 @@
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
-let stream;
+let stream, recorder, videoFile;
 
 const handleStart = () => {
   startBtn.innerText = "Stop Recording";
   startBtn.removeEventListener("click", handleStart);
   startBtn.addEventListener("click", handleStop);
-  const recorder = new MediaRecorder(stream);
+  recorder = new MediaRecorder(stream, { mimeType: "video/webm" });
   console.log("**** before start", recorder);
-  recorder.ondataavailable = (e) => console.log(e);
+
+  // recording이 끝났을때
+  recorder.ondataavailable = (e) => {
+    console.log("recorder done");
+    console.log(e);
+    console.log(e.data);
+
+    // createObjectURL  은 실제 URL이 아니라 녹화된 동영상이 위치한 메모리의 위치를 포인트 한다.
+    videoFile = URL.createObjectURL(e.data);
+    console.log(videoFile);
+    video.srcObject = null;
+    video.src = videoFile;
+    video.loop = true;
+    video.play();
+  };
+
   recorder.start();
   console.log("====== after start", recorder);
 
-  setTimeout(() => {
-    recorder.stop();
-  }, 10000);
+  // setTimeout(() => {
+  //   recorder.stop();
+  // }, 10000);
+};
+
+const handleDownload = () => {
+  const a = document.createElement("a");
+  a.href = videoFile;
+  a.download = "myRecording.webm";
+  document.body.appendChild(a);
+  a.click();
 };
 
 const handleStop = () => {
-  startBtn.innerText = "Start Recording";
+  startBtn.innerText = "Download Recording";
   startBtn.removeEventListener("click", handleStop);
-  startBtn.addEventListener("click", handleStart);
+  recorder.stop();
+  startBtn.addEventListener("click", handleDownload);
 };
 
 const init = async () => {
